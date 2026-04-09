43f86ccc1641b8d6e24cf35b4229f285e1bc9abd
Conor Westphal <conorwes@users.noreply.github.com>
2026-04-09 14:44:53 -0400

    chore: finally got the collapsible sections working correctly

--- src/topic.cpp
+++ src/topic.cpp
@@ -62,7 +62,8 @@ void add_styled_section(XMLNode *parent, const std::string &title)
     p->SetAttribute("class", "p_Normal");
 }
 
-void add_collapsable_section(XMLNode *parent, const std::string &text, const std::string &toggle_id){
+void add_collapsable_section(XMLNode *parent, const std::string &text, const std::string &toggle_id)
+{
     // 1. The toggle button and label
     auto toggle = add_element(parent, "p");
     toggle->SetAttribute("class", "p_Normal");
@@ -71,13 +72,16 @@ void add_collapsable_section(XMLNode *parent, const std::string &text, const std
     auto img = add_element(toggle, "img");
     img->SetAttribute("id", (toggle_id + "_ICON").c_str());
     img->SetAttribute("class", "dropdown-toggle-icon");
-    img->SetAttribute("style", "margin: 0; width: 0.938in;height: 0.0938in;border:none;");
+    img->SetAttribute("style", "margin: 0; width: 0.0938in;height: 0.0938in;border:none;");
     img->SetAttribute("src", "../../Resources/Images/hmtoggle_plus1.gif");
 
-    auto a = add_element(img, "a", text.c_str());
+    auto a = add_element(img, "a");
     a->SetAttribute("class", "dropdown-toggle");
-    a->SetAttribute("style", "font-style: normal;font-weight: normal;color: #000000;background-color: transparent;text-decoration: none;"); // TODO - Update this to use the heading style
+    a->SetAttribute("style", "font-style: normal;font-weight: normal;color: #000000;background-color: transparent;text-decoration: none;"); // TODO - do we need any of the styling at this level?
     a->SetAttribute("href", ("javascript:HMToggle('toggle,'" + toggle_id + "','" + toggle_id + "_ICON')").c_str());
+
+    auto span = add_element(a, "span", text.c_str());
+    span->SetAttribute("class", "f_Heading2");
 
     // 2. The toggle body
     auto div = add_element(parent, "div");
@@ -114,12 +118,37 @@ auto Topic::create_topic(std::string prev_topic, std::string next_topic) -> bool
     meta_k->SetAttribute("name", "keywords");
     meta_k->SetAttribute("content", m_keyword.c_str());
 
+    auto meta_h = add_element(head, "meta");
+    meta_h->SetAttribute("http-equiv", "X-UA-Compatible");
+    meta_h->SetAttribute("content", "IE=edge");
+
     auto link = add_element(head, "link");
-    link->SetAttribute("rel", "stylesheet");
+    link->SetAttribute("type", "text/css");
     link->SetAttribute("href", "../../Resources/Stylesheets/default.css");
+    link->SetAttribute("rel", "stylesheet");
 
     auto style = add_element(head, "style", "body { margin: 0px; background: #FFFFFF; }\n");
     style->SetAttribute("type", "text/css");
+
+    // Scripts Section
+    // TODO - Figure out why these scripts require that they be at the same level as the page itself - this is a problem with the script, not the generator
+    auto script_1 = doc.NewElement("script");
+    script_1->SetAttribute("type", "text/javascript");
+    script_1->SetAttribute("src", "Resources/jquery.js");
+    script_1->InsertEndChild(doc.NewText("")); // Force full closing tag
+    head->InsertEndChild(script_1);
+
+    auto script_2 = doc.NewElement("script");
+    script_2->SetAttribute("type", "text/javascript");
+    script_2->SetAttribute("src", "Resources/helpman_settings.js");
+    script_2->InsertEndChild(doc.NewText("")); // Force full closing tag
+    head->InsertEndChild(script_2);
+
+    auto script_3 = doc.NewElement("script");
+    script_3->SetAttribute("type", "text/javascript");
+    script_3->SetAttribute("src", "Resources/helpman_topicinit.js");
+    script_3->InsertEndChild(doc.NewText("")); // Force full closing tag
+    head->InsertEndChild(script_3);
 
     // Body Section
     auto body = add_element(root, "body");
@@ -152,61 +181,104 @@ auto Topic::create_topic(std::string prev_topic, std::string next_topic) -> bool
         add_element(td_right, "a", "Next")->SetAttribute("href", next_topic.c_str());
     }
 
+    auto placeholder = add_element(body, "table");
+    placeholder->SetAttribute("style", "width: 100%;border: none;border-spacing: 0px;");
+
+    auto tr_placeholder = add_element(placeholder, "tr");
+    tr_placeholder->SetAttribute("style", "vertical-align: top;");
+
+    auto td_placeholder = add_element(tr_placeholder, "td");
+    td_placeholder->SetAttribute("style", "text-align: left;padding: 5px;");
+
+    auto id = "toggle0186a";
+    int id_suffix = 1;
+    auto num_toggles = 0;
+
     // If this is an "Available Objects" or "Available Functions" page, just put in the placeholder text
     if (m_type == TopicType::available_dos || m_type == TopicType::available_funcs)
     {
-        add_element(body, "p", g_placeholder.c_str())->SetAttribute("class", "p_Normal");
+        add_element(td_placeholder, "p", g_placeholder.c_str())->SetAttribute("class", "p_Normal");
     }
     else
     {
         // Content Generation
-        add_styled_section(body, "Description");
+        add_styled_section(td_placeholder, "Description");
 
         if (m_type == TopicType::domain_object)
         {
-            add_element(body, "b", "Inheritance Hierarchy:");
-            add_element(body, "p", g_placeholder.c_str())->SetAttribute("class", "p_Normal");
-            add_styled_section(body, "Available In Editions:");
+            add_element(td_placeholder, "b", "Inheritance Hierarchy:");
+            add_element(td_placeholder, "p", g_placeholder.c_str())->SetAttribute("class", "p_Normal");
+            add_styled_section(td_placeholder, "Available In Editions:");
 
             // Collapsible sections
-            auto id = "toggle0186a";
-            int id_suffix = 1;
             for (const auto &section : {"Constructors", "Properties", "Methods"})
             {
-                /*auto det = add_element(body, "details");
-                auto sum = add_element(det, "summary");
-                auto h = add_element(sum, "h2");
-                h->SetAttribute("class", "p_Heading2");
-                add_element(h, "span", section)->SetAttribute("class", "f_Heading2");
-                add_element(det, "p", g_placeholder.c_str())->SetAttribute("class", "p_Normal");*/
-                add_collapsable_section(body, section, id + std::to_string(id_suffix));
+                add_collapsable_section(td_placeholder, section, id + std::to_string(id_suffix));
                 id_suffix++;
+                num_toggles++;
             }
         }
         else if (m_type == TopicType::property)
         {
-            add_styled_section(body, "Attributes");
-            add_styled_section(body, "Syntax");
+            add_styled_section(td_placeholder, "Attributes");
+            add_styled_section(td_placeholder, "Syntax");
         }
         else if (m_type == TopicType::method || m_type == TopicType::function || m_type == TopicType::constructor)
         {
             if (m_needs_signature)
             {
-                add_styled_section(body, "Signature");
-                add_styled_section(body, "Arguments");
+                add_styled_section(td_placeholder, "Signature");
+                add_styled_section(td_placeholder, "Arguments");
                 if (m_type != TopicType::constructor)
-                    add_styled_section(body, "Return Value");
-                add_styled_section(body, "Syntax");
+                    add_styled_section(td_placeholder, "Return Value");
+                add_styled_section(td_placeholder, "Syntax");
             }
             else
             {
-                add_styled_section(body, "Overload List");
+                add_styled_section(td_placeholder, "Overload List");
             }
         }
 
-        add_styled_section(body, "See also");
+        add_styled_section(td_placeholder, "See also");
     }
 
+    std::string script_text = "\nif (window.topicInitScriptAvailable) {"
+                              "\nfunction HMInitToggleStates() {";
+
+    for (auto i = 0; i < num_toggles; i++)
+    {
+        auto toggle_id = std::string(id) + std::to_string(i + 1);
+        script_text += std::string("\nHMInitToggle('" + toggle_id + "_ICON','hm.type','dropdown','hm.state','0','hm.src0','hmtoggle_plus0.gif','hm.src1','hmtoggle_plus1.gif','onclick','HMToggle(\\'toggle\\',\\'" + toggle_id + "\\',\\'" + toggle_id + "_ICON\\')');");
+        script_text += std::string("\nHMInitToggle('" + toggle_id + "','hm.type','dropdown','hm.state','0');");
+    }
+
+    script_text += "\n}"
+                   "\nvar checkTimer=setTimeout(\"HMInitToggleStates()\",0);"
+                   "\n}\n";
+
+    auto script_final = add_element(body, "script");
+    script_final->SetAttribute("type", "text/javascript");
+
+    // 1. Create the opening '/*' as a plain text node
+    script_final->InsertEndChild(doc.NewText("/*"));
+
+    // 2. Create the CDATA node.
+    // To get '/*<![CDATA[*/', the CDATA content must START with '*/'
+    // To get '/*]]>*/', the CDATA content must END with '/*'
+    std::string cdata_content = "*/\n" + script_text + "\n/*";
+    auto cdata_node = doc.NewText(cdata_content.c_str());
+    cdata_node->SetCData(true);
+    script_final->InsertEndChild(cdata_node);
+
+    // 3. Create the closing '*/' as a plain text node
+    script_final->InsertEndChild(doc.NewText("*/"));
+
     // File I/O
-    return doc.SaveFile(m_filename.c_str()) == XML_SUCCESS;
+    XMLPrinter printer(0, false);
+    doc.Print(&printer);
+    std::ofstream of(m_filename.c_str());
+    of << printer.CStr();
+    of.close();
+
+    return true;
 }
